37e2f92f4d75b8e5fecb02c9c8e938638dfb1221
kevlag <hello@lagier-kevin.com>
2013-03-26 22:15:25 +0100

    menu

---
+++ app/Views/assets/menu.php
@@ -0,0 +1,12 @@
+<div id="top"></div>
+<div id="menu">
+	<nav>
+		<h1>We Are Full Linked</h1>
+		<ul>
+			<li><a href="/" class="selected" >Work</a></li>
+			<li><a href="resume" class="notSelected" >About</a></li>
+			<li><a href="about" class="notSelected" >Contact</a></li>		
+		</ul>
+	</nav>	
+</div>
+
