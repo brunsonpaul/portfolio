26ea1f163e4f119789dd28fb20393d66172b8c14
kevlag <hello@lagier-kevin.com>
2013-03-26 20:57:50 +0100

    maj

---
+++ app/Views/assets/head.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title></title>
+    <meta name="description" content="">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <meta name="author" content="Kevin LAGIER & Florentin MONTEIL" />
+    <meta name="keywords" content="" >
+    <meta name="description" content="" >
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="css/img/favicon.png" />
+
+    <link rel="stylesheet" href="public/css/soSteezy.css" type="text/css" media="screen"  />
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="public/js/lib/modernizr-2.5.3.min.js"></script>
+    <script type="text/javascript" src="public/js/lib/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="public/js/lib/supersized.3.2.7.js"></script>
+    <script type="text/javascript" src="public/js/main.js"></script>
+    
+</head>
+
+
