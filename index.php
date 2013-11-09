794e17c5acee7ab6e486c4a6fbb67c55b9069669
Florentin Monteil <florentin.monteil@gmail.com>
2013-11-09 15:45:21 +0100

    Merge

---
+++ index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Kévin Lagier</title>
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <meta name="author" content="Kévin LAGIER" />
+    <meta name="keywords" content="Kévin LAGIER, portfolio, web, design, Hetic, graphic design, webdesign, websites, communication, project management, ergonomics, UX, responsive, digital" />
+    <meta name="description" content="Personal portfolio Kévin LAGIER, French Graphic Designer From Annecy, student at Hetic in Paris" />   
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="img/identity/favicon.png" />
+    <link rel="stylesheet" href="assets/css/soSteezy.css" type="text/css" media="screen"  />
+</head>
+
+<body class="userSelect">
+
+    <!--
+    <div id="loader">
+        <div>
+            <img src="assets/img/identity/logo-kevin-lagier-black.svg" alt="logo Kévin Lagier"> 
+            <p>Loading ...</p>
+            <span>      
+                <span></span>
+            </span>
+        </div>
+    </div> !--> 
+
+    <div id='borderTop' class="border close"></div>
+    <div id='borderBottom' class="border close"></div>
+    <div id='borderLeft' class="border close"></div>
+    <div id='borderRight' class="border close"></div>
+
+    <nav class="button-menu">
+        <a href="#">
+            <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="18px" height="7px" viewBox="0 0 17 7.016" enable-background="new 0 0 17 7.016" xml:space="preserve">
+                <rect fill="#b59663" width="18" height="1"/>
+                <rect y="3" fill="#b59663" width="18" height="1"/>
+                <rect y="6" fill="#b59663" width="18" height="1"/>
+            </svg>
+        </a>
+    </nav> 
+
+    <div class="projects-container dragCursor">
+
+
+        <div id='home' class="project" data-project="home">
+            <header>
+                <div class="wave1">        
+                    <img src="assets/img/identity/wave-1.svg" alt=""> 
+                </div>
+                <div class="wave2">        
+                    <img src="assets/img/identity/wave-2.svg" alt=""> 
+                </div>
+                <div id="header-content">
+                    <h1>
+                        <span>Kévin Lagier</span>
+                        <img src="assets/img/identity/logo-kevin-lagier.svg" alt="logo Kévin Lagier"> 
+                    </h1>
+                    <p>
+                        I'm a 22 years old student at Hétic & here,<br/>
+                        you can find my work selection
+                    </p>
+                    <div id="discover">Drag to discover projects</div>
+                    <ul id="contact">
+                        <li><a href="mailto:hello@lagier-kevin.com">Say Hello</a></li><!--
+                        !--><li><a href="assets/media/resume.pdf" target="_blank" >My Resume</a></li>
+                    </ul>
+                    <ul id="social">
+                        <li>
+                            <a id='twitter' href="https://twitter.com/LagierKevin" target="_blank">L</a>
+                        </li><!--
+                        !--><li>
+                            <a id="behance" href="http://www.behance.net/KevinLagier" target="_blank">E</a>
+                        </li><!--
+                        !--><li>
+                            <a id="dribbble" href="http://dribbble.com/kevlag" target="_blank">D</a>
+                        </li><!--
+                        !--><li>
+                            <a id="linkedin" href="http://www.linkedin.com/profile/view?id=175237599&trk=hb_tab_pro_top" target="_blank">I</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="copyright">
+                    <p>© 2013 Kévin Lagier. Developed by Florentin Monteil</p>
+                </div>
+            </header>
+        </div><!--
+        --><!--<div id='tdfProject' class="project" data-project="100ansdetour">
+        </div><!--
+        --><div id='theWholeProject' class="project" data-project="the-whole">
+            <!-- <div>
+                <p>
+                    The Whole est une interface qui réuni des flux d'informations issus de sources variées et les présente sous la forme d'un mur d'informations.<br/>Ce projet présente la particularité de la mise en place d'un système d'unification des APIs.
+                </p>       
+                <img src="assets/img/the-whole/bg/the-whole-space.svg" alt="Social Coke"> 
+            </div> -->
+        </div><!--
+        --><div id='socialCokeProject' class="project" data-project="social-coke">
+                <div>
+                    <h1>
+                        <span>Partech</span>
+                        <img src="assets/img/social-coke/bg/social-coke.svg" alt="Social Coke"> 
+                    </h1>
+                    <p>#SocialCoke, ou comment transformer<br/>l’écriture sociale en message publicitaire.</p>
+                    <a href="#" class="enter-button">
+                        <span>View the case study</span>
+                        <span>View the case study</span>
+                    </a>
+                </div>    
+            <!--<img style='position:absolute; top:7%; right:12%;'src='assets/img/social-coke/bg/monkey-bg.png'>
+            <img style='position:absolute; top:58%; left:41%;'src='assets/img/social-coke/bg/patate-bg.png'>
+            <img style='position:absolute; bottom:23%; right:4%;'src='assets/img/social-coke/bg/drink-bg.png'>
+            <img style='position:absolute; top:13%; left:29%;'src='assets/img/social-coke/bg/hello-bg.png'>
+            <img style='position:absolute; bottom:38%; right:41%;'src='assets/img/social-coke/bg/goutte-bg.png'>
+            <img style='position:absolute; top:37%; right:38%;'src='assets/img/social-coke/bg/pointRose-bg.png'>
+            <img style='position:absolute; bottom:43%; right:33%;'src='assets/img/social-coke/bg/pointVert-bg.png'>
+            <img style='position:absolute; bottom:36%; right:25%;'src='assets/img/social-coke/bg/pointJaune-bg.png'>!-->
+        </div><!--
+
+        --><div id='partechProject' class="project" data-project="partech">
+                <div>
+                    <h1>
+                        <span>Partech</span>
+                        <img src="assets/img/partech/bg/partech.svg" alt="Partech"> 
+                    </h1>
+                    <p>
+                        Corporate website for Partech, a design office specializing<br/>
+                        in manufactured chairlifts, in industrial<br/>
+                        equipment and mountain.
+                    </p>
+                    <a href="#" class="enter-button">
+                        <span>View the case study</span>
+                        <span>View the case study</span>
+                    </a>
+                </div>
+        </div><!--
+        
+        --><div id='footer' class="project" data-project="footer">
+            <footer>
+                <div class="wave1">        
+                    <img src="assets/img/identity/wave-1.svg" alt=""> 
+                </div>
+                <div class="wave2">        
+                    <img src="assets/img/identity/wave-2.svg" alt=""> 
+                </div>
+                <img id='footerLogo' src="assets/img/identity/logo-kevin-lagier.svg" alt="logo Kévin Lagier"> 
+            </footer>
+        </div>
+    </div>
+    <div class="transition-div">
+        
+    </div>
+    <div class="case-study-container">
+
+        <?php require('./templates/social-coke.php') ?>
+
+    </div>
+
+    <!-- build:js scripts/vendor.js -->
+    <script src="scripts/vendor/jquery/jquery.js"></script>
+    <script src="scripts/vendor/underscore/underscore.js"></script>
+    <script src="scripts/vendor/backbone/backbone.js"></script>
+    <script src="scripts/vendor/dragdealer.js"></script>
+    <!-- endbuild -->
+
+    <!-- build:js({.tmp,app}) scripts/main.js -->
+    <script src="scripts/main.js"></script>
+    <script src="scripts/routes/application.js"></script>
+    <script src="scripts/views/home.js"></script>
+    <script src="scripts/views/100ansdetour.js"></script>
+    <script src="scripts/views/converseChallenge.js"></script>
+    <script src="scripts/views/partech.js"></script>
+    <script src="scripts/views/socialCoke.js"></script>
+    <script src="scripts/views/theWhole.js"></script>
+    <script src="scripts/collections/application.js"></script>
+    <!-- endbuild -->
+
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-29119748-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+
+</body>
+</html>
---
+++ templates/social-coke.php
@@ -0,0 +1,166 @@
+<section id='socialCoke' data-case-study="social-coke">
+    <article id='socialCoke-introduction'>
+        <div class="page-content">
+            <ul>
+                <li>
+                    <span>Date :</span>
+                    Decembre 2012
+                </li><!--
+                !--><li>
+                    <span>Client :</span>
+                    Coca Cola
+                </li><!--
+                !--><li>
+                    <span>Role :</span>
+                    Design - Intégration
+                </li>
+            </ul>
+            <h1>
+                Social Coke
+                <img src='./assets/img/social-coke/social-coke-logo.svg' alt='Social Coke Logo'>
+            </h1>
+            <p>
+                Social Coke est une (fausse) campagne digitale à l’échelle internationale pour la marque Coca Cola, basée sur un concept innovant de site web collaboratif avec Twitter.
+                Le site plonge ses internautes dans une expérience riche, captivante, amusante où ils pourront découvrir différents modules à base de jeux ou de contenus dynamiques.
+            </p>
+            <a href='http://lagier-kevin.com/social-coke/' target='_blank'>                    
+                <img src='./assets/img/social-coke/social-coke-link.svg' alt='Social Coke Link'>
+                <img src='./assets/img/social-coke/social-coke-link-hover.svg' alt='Social Coke Link hover'>
+            </a>
+        </div>
+        <img class='goutte1' src='./assets/img/social-coke/goutte1.png' alt=''>
+        <img class='goutte2' src='./assets/img/social-coke/goutte2.png' alt=''>
+        <img class='goutte3' src='./assets/img/social-coke/goutte3.png' alt=''>
+        <img class='goutte4' src='./assets/img/social-coke/goutte4.png' alt=''>
+        <img class='goutte5' src='./assets/img/social-coke/goutte5.png' alt=''>
+    </article>
+
+    <article id='socialCoke-context'>
+        <img id='cloud1' src='./assets/img/social-coke/cloud-2.svg' alt=''>
+        <img id='cloud2' src='./assets/img/social-coke/cloud.svg' alt=''>
+        <img id='cloud3' src='./assets/img/social-coke/cloud-3.svg' alt=''>
+        <img id='cloud4' src='./assets/img/social-coke/cloud-4.svg' alt=''>
+        <div class="page-content">
+            <h2>The Context</h2>
+            <h3>Coca Cola, une marque active et créative</h3>
+            <p>
+                Au fils des années l’entreprise Coca Cola a façonné son image et sa notoriété à grand coup de campagnes publicitaires de grande ampleur, marquantes par leur originalité et par la qualité des réalisations.<br/>
+                Au fils des siècles la marque a par ailleurs souvent fait appel à des artistes et designers de renom pour réaliser des concepts visuels encore gravés dans les esprits.
+            </p><!--
+            !--><p>
+                Aujourd’hui Coca Cola cherche à poursuivre cette tradition de campagnes innovantes et créatives en prenant en compte les nouveaux enjeux de 2012 et en particulier le canal internet.<br/>
+                La marque cherche en particulier à s’adresser à une cible jeune, sensible aux innovations digitales, pour renforcer sa notoriété et son image de marque innovante.                
+            </p>
+            <img id='coca-cola-logo' src='./assets/img/social-coke/coca-cola.svg' alt='Coca Cola logo'>   
+             <p>
+                <em>Une marque active et créative en terme de campagne de communication :</em>
+                <b>•</b><!-- !--><span>Des thèmes récurrents dans la communication Coca Cola : l’émerveillement, le rêve, le merveilleux</span><br/>
+                <b>•</b><!-- !--><span>Un lien fort entre la marque (campagnes, packaging, logo) et l’art</span><br/>
+                <b>•</b><!-- !--><span>Une marque qui s’adresse à une cible plutôt jeune, par conséquent sensible au digital et à l’innovation de manière générale</span><br/>
+            </p><!--
+            !--><p>
+                <em>Image :</em>
+                Entretenir et renforcer l’image d’une marque jeune et innovante, proche des milieux artistiques et active dans le domaine du digital.
+                <em>Notoriété :</em>
+                Entretenir la notoriété de la marque sur les réseaux sociaux et en particulier sur Twitter.    
+           </p>     
+        </div>
+    </article>
+
+    <article id='socialCoke-idea'>
+        <div class="page-content">
+            <h2>The Idea</h2>
+            <h3>Campagne digitale pour Coca Cola</h3>    
+            <p>
+                Le site propose aux internautes de découvrir un vaste univers très illustré. Au cœur de cet univers se trouve une bouteille dont les formes se dessinent peu à peu avec les noms des internautes qui tweet avec le # de la campagne (#socialcoke). Le but premier de cette page est donc d’inciter les internautes à parler de la marque. Ils ont par ailleurs la possibilité de le faire directement depuis la page d’accueil.
+            </p>
+            <a href='#'>
+                <img src='./assets/img/social-coke/btn-tweet.svg' alt=''>
+                <img src='./assets/img/social-coke/btn-tweet-hover.svg' alt=''>
+            </a>
+    </article>
+
+    <article id='socialCoke-interest'>
+        <div class="page-content">
+            <h2>Quel intérêt pour l'internaute ?</h2>
+            <p>
+                Lorsque l’internaute se rend sur le site, il plonge au coeur d’un univers graphique qui capte sa curiosité et son émerveillement. On s’appuie ainsi sur une constante de la communication Coca Cola, à savoir la place primordiale qu’occupent l’émerveillement et l’émotionnel.<br/><br/>
+            </p><!--
+            !--><p>
+                Plus concrètement, l’interaction de l’internaute avec le site se fait principalement sur la base du tweet. Pour chaque tweet posté avec #socialcoke, le pseudo de l’internaute vient compléter le design de la bouteille. Il apparaît alors directement sur la bouteille et découvre un peu plus de sa forme.<br/><br/>
+            </p><!--
+            !--><p>
+                Par ailleurs, les tweets ont une seconde fonction dans notre campagne. Ils permettent de construire l’univers Coca Cola autour de l’événement principal. Les internautes prennent ainsi partie à la construction de l’univers « décalé » qui se dessine peu à peu.
+            </p>
+        </div>
+    </article>
+            
+    <article id='socialCoke-universe'>
+        <div class="page-content">
+            <h2>The Universe</h2>
+            <h3>Propose à l’internaute une expérience riche, captivante et amusante</h3>    
+            <p>
+                Cet univers entourant la bouteille présente la marque Coca Cola sous l’angle de l’art. Il est composé à la manière d’une infographie, présentant des historiques graphiques de la marque, l’évolution de son design, ou encore la forme de ses bouteilles au fil des années, le tout présenté de manières ludique.
+            </p><!--
+            !--><p>
+                L’univers est volontairement très illustré, sans cadre défini pour laisser une très grande liberté à la navigation de l’internaute.<br/>Un soin particulier a été accordé aux détails dans les illustrations, les animations et tous autres rendus visuels qui permettent d’attiser, la curiosité et l’enthousiasme du visiteur.            
+            </p>
+            <img src='./assets/img/social-coke/oiseaux.svg' alt=''> 
+        </div>                  
+    </article>
+
+    <article id='socialCoke-bottle'>
+        <div class="page-content">
+            <h2>Et quand la bouteille est pleine ?</h2>
+            <p>
+                La campagne se termine lorsque la bouteille est pleine. Pour autant tout ne s’arrête pas là. Il s’agit de remercier les participants et de profiter de la plus grande notoriété du site.<br/><br/>
+            </p><!--
+            !--><p>
+                La bouteille sera donc réellement produite sous forme d’une nouvelle édition collector "Socialbottle". Elle sera offerte aux participants à la campagne et une possibilité sera donnée aux internautes de la commander sur le site.<br/><br/>
+            </p><!--
+            !--><p>
+                Le site sera par ailleurs disponible dans son intégralité. Les visiteurs pourront donc profiter pleinement de l’expérience, à la manière d’une infographie présentant les liens forts entre Coca Cola et le monde de l’art.  
+            </p>
+        </div>
+    </article>
+
+    <article id='socialCoke-madeWith'>
+        <div class="page-content">
+            <h2>A project made with ...</h2>
+            <ul>
+                <li>
+                    <a href='http://bastien-lardeux.com/' target='_blank'>Maxime Bigot</a>
+                    <a href='https://twitter.com/dolly_pr4ne' target='_blank'>L</a>
+                    Développement</br>
+                    Illustration
+                </li><!--
+                !--><li>
+                    <a href='http://www.linkedin.com/pub/l%C3%A9o-denis/44/423/454' target='_blank'>Léo Denis</a>
+                    <a href='https://twitter.com/lo_denis' target='_blank'>L</a>
+                    Développement<br/>(Php/Javascript)
+                </li><!--
+                !--><li>
+                    <a href='http://mariongeorges.com/' target='_blank'>Marion Georges</a>
+                    <a href='https://twitter.com/MarionGeorges_' target='_blank'>L</a>
+                    Développement,<br/>Stratégie Marketing
+                </li><!--
+                !--><li>
+                    <a href='http://www.nicolasmatt.com/' target='_blank'>Nicolas Matt</a>
+                    <a href='https://twitter.com/nicomatt89' target='_blank'>L</a>
+                    Chef de projet,<br/>Conception
+                </li><!--
+                !--><li>
+                    <a href='http://www.mahedineyahia.fr/' target='_blank'>Mahedine Yahia</a>
+                    <a href='https://twitter.com/mahedou' target='_blank'>L</a>
+                    Design, typographie<br/>illustration
+                </li>
+            </ul>
+        </div>
+        <img id='poisson' src='./assets/img/social-coke/poisson.svg' alt=''>                  
+    </article>
+            
+    <footer>
+        <p>That’s all folk</p>
+        <p>Thank you for watching</p>
+    </footer>
+</section>
