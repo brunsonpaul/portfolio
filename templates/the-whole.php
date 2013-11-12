0998b6ad717d9e179aa0cd1caf000baead23c23d
Florentin Monteil <florentin.monteil@gmail.com>
2013-11-12 20:55:19 +0100

    Merge

---
+++ templates/the-whole.php
@@ -0,0 +1,136 @@
+<section id='theWhole' data-case-study="the-whole">
+    <article id='theWhole-introduction'>
+        <div class="page-content">
+            <ul>
+                <li>
+                    <span>Date :</span>
+                    March 2013
+                </li><!--
+                !--><li>
+                    <span>Client :</span>
+                    The Whole
+                </li><!--
+                !--><li>
+                    <span>Role :</span>
+                    Design
+                </li>
+            </ul>
+            <h1>
+                <img src='./assets/img/the-whole/the-whole-logo.png' alt="The Whole logo">
+                <span>The Whole</span>
+            </h1>
+            <p>
+                The Whole est une interface qui réuni des flux d'informations issus de sources variées et les présente sous la forme d'un mur d'informations.<br/>The Whole est pour le moment un projet en phase de conception.
+            </p>
+            <img src='./assets/img/the-whole/the-whole-mba.png' alt=''>
+        </div>
+    </article>
+    
+    <article id='theWhole-concept' >
+        <div class='page-content'>
+            <h2>The Concept</h2>
+            <h3>Connect and follow the world</h3>
+            <p>
+                Le concept de The Whole est de proposer une plateforme qui unifie tout vos services sur une seul et même interface. Réseaux sociaux, flux rss, musique ou tout autre service serait disponible sur la même application et proposerait les mêmes fonctionnalités.
+            </p><!--
+            !--><p>
+                The Whole est pour le moment qu’un prototype, qui a été réalisé avec uniquement 6 services (Facebook, Twitter, Pinterest, Sound Cloud, Last Fm, Flux Rss). Mais il a été pensé que n’importe quel autre service pourrait être intégré.
+            </p>   
+            <img src='./assets/img/the-whole/the-whole-concept.png' alt=''>
+        </div>
+    </article>
+
+    <article id='theWhole-interface' >
+        <div class='page-content'> 
+            <h2>The interface</h2>            
+                <p>
+                    L’interface propose une navigation horizontale afin de présenter de façon clair, un nombre plus important de post qu’une navigation verticale aurait pu permettre.
+                </p>
+            <div id='theWhole-interface-content'>
+                <img src='./assets/img/the-whole/the-whole-interface.png' alt=''>
+                <ul>
+                    <li>
+                        <img src='./assets/img/the-whole/the-whole-bookmark.png' alt=''>
+                        <h3>Bookmarks</h3>
+                        <p>
+                            Sauvegardé vos posts favori en cliquant sur le bookmark qui apparait au survol du post.
+                        </p>
+                    </li><!-- 
+                    !--><li>
+                        <img src='./assets/img/the-whole/the-whole-twInfos.png' alt=''>
+                        <h3>News feed</h3>
+                        <p>
+                            La source de chaque post est identifiable grâce au design qui s’adapte en fonction du type de post.
+                        </p>
+
+                    </li><!-- 
+                    !--><li>
+                        <img src='./assets/img/the-whole/the-whole-filtre.png' alt=''>
+                        <h3>Filtre du flux</h3>
+                        <p>
+                            Contrôlé très facilement votre flux d’actualité grâce au filtre de la barre du menu.
+                        </p>
+                    </li>
+            </div>
+            <div id='theWhole-menu'>
+                <img src='./assets/img/the-whole/the-whole-menu.png' alt=''>
+                <img src='./assets/img/the-whole/the-whole-safari.png' alt=''>
+                <img src='./assets/img/the-whole/arrow-palette.png' alt=''>
+                <img src='./assets/img/the-whole/arrow-api.png' alt=''>
+                <img src='./assets/img/the-whole/arrow-rss.png' alt=''>
+                <img src='./assets/img/the-whole/arrow-bookmarks.png' alt=''>
+                <img src='./assets/img/the-whole/arrow-profil.png' alt=''>
+            </div>            
+        </div>
+    </article>
+
+    <article id='theWhole-element' >
+        <div class='page-content'>
+            <span>
+                <h2><b>Clear</b>Elements</h2>            
+                <p>
+                    Il a été nécessaire de réaliser un ensemble d’éléments ayant un thème visuel commun pouvant marcher séparément et ensemble afin de créer une ambiance riche et interactive.<br/><br/>
+                    Chaque éléments a été réfléchi avant leur conception. J’ai ensuite eu le plaisir de concevoir et apporter de petits détails à chacun de ces composants.
+                </p>
+            </span><!--
+            !--><img id='element-img' src='./assets/img/the-whole/element.png' alt=''>
+        </div>
+    </article>
+
+    <article id='theWhole-madeWith'>
+        <div class="page-content">
+            <img src='./assets/img/the-whole/the-whole-macbook-air.png' alt=''>
+            <h2>A project made with ...</h2>
+            <ul>
+                <li>
+                    <a href='http://robinlambert.fr/' target='_blank'>Robin Lambert</a>
+                    <a href='https://twitter.com/Robin_Lambert' target='_blank'>L</a>
+                    Ux Ergonomie<br/>Conception
+                </li><!--
+                !--><li>
+                    <a href='http://www.nicolasmatt.com/' target='_blank'>Nicolas Matt</a>
+                    <a href='https://twitter.com/nicomatt89' target='_blank'>L</a>
+                    Chef de projet<br/>Conception
+                </li><!--
+                !--><li>
+                    <a href='http://florentin-monteil.com/' target='_blank'>Florentin Monteil</a>
+                    <a href='https://twitter.com/FloMonteil' target='_blank'>L</a>
+                    Développement<br/>(Fat Free, Api)
+                </li><!--
+                !--><li>
+                    <a href='http://cs-work.com/' target='_blank'>Constance Souville</a>
+                    <a href='https://twitter.com/ConstanceSou' target='_blank'>L</a>
+                    Integration<br/>Conception
+                </li>
+            </ul>
+        </div>
+    </article>
+
+    <footer>
+        <p>That’s all folk</p>
+        <p>Thank you for watching</p>
+    </footer>
+</section>
+
+</body>
+</html>
