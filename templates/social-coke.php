850d0c445285f396a13c30eb7386f2a0e8460700
Lagier Kévin <hello@lagier-kevin.com>
2014-01-19 20:02:08 +0100

    transition social coke

--- templates/social-coke.php
+++ templates/social-coke.php
@@ -1,6 +1,4 @@
-<div class="transition-social-coke" style="position:fixed; z-index:-1; top:0; left:0; bottom:0; right:0; background:blue;">
-    Test de transition ....
-</div>
+<div id="transition-social-coke"></div>
 <section id='socialCoke' data-case-study="social-coke">
     <nav class="pagination socialCoke">
         <ul>
