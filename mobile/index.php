f63e7411bf6d9065c3826d4b6452d9ad6b5ed8f0
Lagier Kévin <hello@lagier-kevin.com>
2014-01-20 14:18:39 +0100

    favicon

--- mobile/index.php
+++ mobile/index.php
@@ -5,32 +5,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <title>Kévin Lagier's Portfolio - mobile</title>
+    <title>Kévin Lagier</title>
 
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
     <meta name="author" content="Kévin LAGIER" />
-    <meta name="description" content="Kevin Lagier's Portfolio, graphic design, art direction, student at Hétic" />
-    <meta name="keywords" content="kevin, lagier, portfolio, hetic, student, étudiant, web, digital, design, graphic" />
+    <meta name="keywords" content="Kévin LAGIER, portfolio, web, design, Hetic, graphic design, webdesign, websites, communication, project management, ergonomics, UX, responsive, digital" />
+    <meta name="description" content="Personal portfolio Kévin LAGIER, French Graphic Designer From Annecy, student at Hetic in Paris" />   
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="../assets/img/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="../assets/img/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="../assets/img/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="../assets/img/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="../assets/img/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="../assets/img/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="../assets/img/apple-touch-icon-152x152.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon-196x196.png" sizes="196x196">
-    <link rel="icon" type="image/png" href="../assets/img/favicon-160x160.png" sizes="160x160">
-    <link rel="icon" type="image/png" href="../assets/img/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="../assets/img/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="../assets/img/favicon-16x16.png" sizes="16x16">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <link rel="icon" type="image/png" href="../assets/img/identity/favicon.png">
 
-    <link rel="stylesheet" href="../assets/css/soSteezy-mobile.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../assets/css/soSteezy-mobile.css" type="text/css" media="screen" x/>
 </head>
 
 <body>
