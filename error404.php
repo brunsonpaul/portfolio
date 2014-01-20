218316e262f1cfe8c51af869de1390c27f1c1166
Lagier Kévin <hello@lagier-kevin.com>
2014-01-20 12:49:50 +0100

    favicon

---
+++ error404.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Kévin Lagier's Portfolio - mobile</title>
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <meta name="author" content="Kévin LAGIER" />
+    <meta name="description" content="Kevin Lagier's Portfolio, graphic design, art direction, student at Hétic" />
+    <meta name="keywords" content="kevin, lagier, portfolio, hetic, student, étudiant, web, digital, design, graphic" />
+
+    <!-- Favicon !-->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon-196x196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="assets/img/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="assets/img/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+
+    <link rel="stylesheet" href="assets/css/soSteezy-error.css" type="text/css" media="screen" />
+</head>
+
+<body>
+
+<div id="header-404">
+    <h1>
+        <span>Kévin Lagier</span>
+        <img src="assets/img/identity/logo-kevin-lagier-black.svg" alt="logo Kévin Lagier"> 
+    </h1>
+    <p>Error 404</p>
+    <a href='/'>Go back my friend</a>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -24,7 +24,24 @@ setcookie('cookie', $cookie);
 
     <meta name="description" content="Kevin Lagier's Portfolio, graphic design, art direction, student at Hétic" />
     <meta name="keywords" content="kevin, lagier, portfolio, hetic, student, étudiant, web, digital, design, graphic" />
-    <link rel="icon" type="image/x-icon" href="assets/img/identity/favicon.png" />
+
+    <!-- Favicon !-->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon-196x196.png" sizes="196x196">
+    <link rel="icon" type="image/png" href="assets/img/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="assets/img/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+
     <link rel="stylesheet" href="assets/css/soSteezy.css" type="text/css" media="screen" />
 
     <script src="scripts/views/pace.js"></script>
