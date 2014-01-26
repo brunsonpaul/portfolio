cad11d6aa0a4377db6c13705e9da33b8dc488576
Lagier Kévin <hello@lagier-kevin.com>
2014-01-26 22:44:34 +0100

    analytics

--- index.php
+++ index.php
@@ -435,7 +435,7 @@ setcookie('cookie', $cookie);
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-29119748-2', 'lagier-kevin.com');
+      ga('create', 'UA-29119748-1', 'lagier-kevin.com');
       ga('send', 'pageview');
         
         $(document).ready(function() {
